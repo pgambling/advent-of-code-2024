7c2cfae5a2aa75668d5f07645fc4c07e5b2703ad
Phil Gambling <pgambling@gmail.com>
2024-12-11 21:29:24 -0600

    Day 11 part 2 SOLVED

--- day11/src/part2.rs
+++ day11/src/part2.rs
@@ -1,32 +1,31 @@
-// use cached::proc_macro::cached;
+use cached::proc_macro::cached;
 
-const BLINKS: usize = 25;
+const BLINKS: usize = 75;
 
 pub fn solve(input: &[String]) -> usize {
     load_input(input)
         .iter()
-        .map(|stone| count_stones(*stone, BLINKS))
+        .map(|stone| count_stones(*stone, 0, BLINKS))
         .sum()
 }
-// #[cached]
-fn count_stones(stone: usize, blinks: usize) -> usize {
-    let mut stones = vec![stone];
-    for _ in 0..blinks {
-        let len = stones.len();
-        for index in 0..len {
-            let stone = stones[index];
-            if stone == 0 {
-                stones[index] = 1;
-            } else if has_even_digits(stone) {
-                let (left, right) = split_even_digit_number(stone);
-                stones[index] = left;
-                stones.push(right);
-            } else {
-                stones[index] = stone * 2024;
-            }
+#[cached]
+fn count_stones(stone: usize, current_blink: usize, total_blinks: usize) -> usize {
+    let mut count = 1;
+    let mut stone = stone;
+    let mut current_blink = current_blink;
+    while current_blink < total_blinks {
+        current_blink += 1;
+        if stone == 0 {
+            stone = 1;
+        } else if !has_even_digits(stone) {
+            stone = stone * 2024;
+        } else {
+            let (left, right) = split_even_digit_number(stone);
+            stone = left;
+            count += count_stones(right, current_blink, total_blinks);
         }
     }
-    stones.len()
+    count
 }
 
 fn has_even_digits(n: usize) -> bool {
